9de3074b504c904183411139968d62ea641d7604
blinky3713 <blinky@foam.space>
2018-03-28 21:33:07 -0400

    add orphans

---
+++ src/Types/Orphans.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Types.Orphans where
+
+import Composite.Record ((:->)(Val))
+import Data.Proxy (Proxy(Proxy))
+import Data.Swagger (ToParamSchema, toParamSchema)
+import Web.HttpApiData (ToHttpApiData, FromHttpApiData)
+
+-- Orphan instances for using `s :-> a` as a @Servant.Capture@ or @Servant.QueryParam@
+instance ToParamSchema a => ToParamSchema (s :-> a) where
+  toParamSchema _ = toParamSchema (Proxy :: Proxy a)
+deriving instance ToHttpApiData a => ToHttpApiData (s :-> a)
+deriving instance FromHttpApiData a => FromHttpApiData (s :-> a)
